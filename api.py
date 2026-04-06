fe3e0babca4c8c575d7f6366edefd4772c032201
Jirjis <jirjis@Jirjiss-MacBook-Pro.local>
2026-04-05 23:28:28 -0400

    Connected auth system to todo API - protected routes

--- api.py
+++ api.py
@@ -1,15 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from sqlalchemy import Boolean, Column, Integer, String, create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
-from fastapi import FastAPI, HTTPException ,Depends
 from fastapi.security import OAuth2PasswordBearer
-
+from jose import jwt
 # Database setup
 engine = create_engine("sqlite:///todos.db")
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
+
+SECRET_KEY = "your-secret-key-keep-it-safe"
+ALGORITHM = "HS256"
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
 
 
 # Database mode
@@ -49,12 +53,22 @@ def get_todos():
 
 
 @app.post("/todos")
-def add_todo(todo: Todo):
+def add_todo(todo: Todo, token: str = Depends(oauth2_scheme)):
+    try:
+
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username = payload.get("sub")
+    except:
+        raise HTTPException(status_code=401 , detail="Invalid token")
+    
     db = SessionLocal()
-    new_todo = TodoModel(task=todo.task, done=todo.done)
+    new_todo = TodoModel(
+        task=todo.task, 
+        done=todo.done)
     db.add(new_todo)
     db.commit()
     db.close()
+    
     return {"message": "Todo added"}
 
 
